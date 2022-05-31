fdb16612e4f0255d00c0076b72cba755e2080c50
joaomateuus <joaomateusmoraisdeamaral@gmail.com>
2022-05-30 21:15:26 -0400

    commiting commit

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,61 @@
 <template>
   <div id="app">
-    <h1>Hello World</h1>
+    <Parallax class="first-scene" :parallax="true"
+    :directon="down" :speedFactor="0.15">
+      <h1 id="title-one">Hello World</h1>
+      <img class="img1" src="./assets/moon.jpg" alt="">
+    </Parallax>
+   <Parallax :parallax="true" :direction="up" :speedFactor="0.15">
+    <div class="container2">
+      <h1 id="third-title">Bye World</h1>
+    </div>
+      <img src="./assets/land.jpg" class="img2" alt="">
+    </Parallax>
   </div>
 </template>
 
 <script>
+import Parallax from 'vue-parallaxy';
 
 export default {
   name: 'App',
-  components: {},
+  components: {
+    Parallax
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
 }
+
+.first-scene{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+#title-one{
+  position: absolute;
+  color: white;
+  font-family: sans-serif;
+  font-size: 90px;
+}
+
+#third-title{
+  position: absolute;
+  color: white;
+  font-family: sans-serif;
+  font-size: 85px;
+}
+
+.container2{
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+
 </style>
